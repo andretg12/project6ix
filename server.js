8655db9df747bcf607bbae1743b9006076037927
Andre Torrealba <andre.torrealba@icloud.com>
2019-12-02 15:18:31 -0500

    trying to get pwa

--- server.js
+++ server.js
@@ -21,7 +21,21 @@ mongo.connect(
 		console.log(err || "Connected to MongoDB");
 	}
 );
-
+app.use(function(environments, status) {
+	environments = environments || ["production"];
+	status = status || 302;
+	return function(req, res, next) {
+		if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+			if (req.headers["x-forwarded-proto"] != "https") {
+				res.redirect(status, "https://" + req.hostname + req.originalUrl);
+			} else {
+				next();
+			}
+		} else {
+			next();
+		}
+	};
+});
 app.use(express.static(`${__dirname}/client/build`));
 app.use(logger("combined"));
 app.use(express.json());
